8e4f345f2fe6f911e3706aae28c7413f20c59f52
ALPHA2204 <thisisalok1334@gmail.com>
2024-12-09 14:52:11 +0530

    Automated Whatsaap Message

---
+++ Automatic Message Sender/main.py
@@ -0,0 +1,9 @@
+'''Here we are going to Send a message automatically without having a do any thing'''
+
+import pywhatkit as pw
+pw.sendwhatmsg('+916388562472',"This messag for you Parth automatic",14,51)
+# Here in the pywhatkit is the a function called sendwhatmsg
+# Which send the message from Web Whatsaap
+# Three Parameter (Mobile Number You Want To Send A message,Message,Hours,Min)  
+
+
